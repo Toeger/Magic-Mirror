560545e57f774b0e242c9cc37be06215d12dca0f
Toeger <Toeger@users.noreply.github.com>
2019-05-02 23:25:09 +0200

    Added Thiefs::Secret_Location::Treasure::Coins encounter.

--- Magic-Mirror.cpp
+++ Magic-Mirror.cpp
@@ -9,7 +9,6 @@
 #include <type_traits>
 
 namespace Ruins {
-	struct Dire_Wolf;
 	struct Black_Key {};
 } // namespace Ruins
 
@@ -21,10 +20,13 @@ namespace Ruins {
 
 namespace Town {
 	struct Blacksmith;
-	struct Rusty_Key {};
 } // namespace Town
 
-/* You know the entrance to the ruins is guarded by Monsters, so you will need a weapon. */
+namespace Ruins {
+	struct Dire_Wolf;
+}
+
+/* You know the entrance to the ruins is guarded by a Dire Wolf, so you will need a weapon. */
 
 class Inventory {
 	friend struct Town::Blacksmith;
@@ -43,6 +45,7 @@ namespace Town {
 		struct Storage_Key {};
 		struct Master_Sword {
 			operator int() {
+				std::cout << "Congratulations! You beat the demo! Now go buy the DLC with microtransactions.\n";
 				return {};
 			}
 		};
@@ -95,6 +98,7 @@ namespace Ruins {
 class Foul_Magic {};
 
 class Necromancer : Foul_Magic {
+	friend class Thiefs;
 	class Key_of_Life {
 		Key_of_Life() {}
 		friend Necromancer;
@@ -111,8 +115,36 @@ class Necromancer : Foul_Magic {
 
 //! TODO: Define a variable named necromancer_key of type Necromancer::Key_of_Life inside main.
 
-/* After taking the Key of Life you enter the Ruins and are met with 2 corridors, each containing a real dragon!
- * You can see that each dragon guards a key and you can almost reach it, but Magic keeps preventing you from reaching them.
+/* After taking the Key of Life you go towards the Dungeon. But before you can enter, you find yourself surrounded by a group of Thiefs.
+ * "We are here on orders of the Thief's Guild. Our spies report that you have a talent for magically getting through locks."
+ * They continue "You were commissioned to produce a Skeleton Key that works on any lock. Also we will be taking that Key of Life."
+ * They explain that you are not forced to comply and are free to choose death instead. You think it might be better to do it. */
+
+class Thiefs {
+	class Secret_Location {
+		struct Unknown_Key {};
+		class Treasure {
+			struct Coins {
+			} coins;
+			Treasure() {}
+			friend class Thiefs;
+		};
+		static Treasure rob(Unknown_Key) {
+			return {};
+		}
+		friend class Thiefs;
+	};
+
+	public:
+	template <class Skeleton_Key>
+	static auto comply(Necromancer::Key_of_Life, Skeleton_Key key) {
+		return Secret_Location::rob(key).coins;
+	}
+};
+//! TODO: Define a variable named coins of type Thiefs::Secret_Location::Treasure::Coins inside main.
+
+/* As you explore the Dungeon, find 2 corridors, each containing a real Dragon!
+ * You can see that each Dragon guards a key and you can almost reach it, but Magic keeps preventing you from reaching them.
  * You must find a way to break through! */
 
 struct Adamantite_Dragon_Layer {
@@ -163,13 +195,14 @@ struct Layer {
 
 int main() {
 	//! TODO: uncomment the below lines and make them compile and run
-	//[[maybe_unused]] auto training_sword =
-	//[[maybe_unused]] auto wooden_shield =
-	//[[maybe_unused]] auto ruin_entrance =
-	//[[maybe_unused]] auto necromancer_key =
-	//[[maybe_unused]] auto adamantite_key =
-	//[[maybe_unused]] auto amethyst_key =
+	//[[maybe_unused]] auto training_sword = //Town::Blacksmith::Training_Sword
+	//[[maybe_unused]] auto wooden_shield = //Town::Blacksmith::Wooden_Shield
+	//[[maybe_unused]] auto ruin_entrance = //Ruins::Dire_Wolf::Ruin_Entrance
+	//[[maybe_unused]] auto necromancer_key = //Necromancer::Key_of_Life
+	//[[maybe_unused]] auto coins = //Thiefs::Secret_Location::Treasure::Coins
+	//[[maybe_unused]] auto adamantite_key = //Adamantite_Dragon_Layer::Adamantite_Key
+	//[[maybe_unused]] auto amethyst_key = //Magic::Amethyst_Key
 	//auto money = Layer::loot_the_layer(adamantite_key, amethyst_key);
 	//auto master_sword = Town::Blacksmith::buy_Master_Sword(money);
-	//return master_sword
+	//return master_sword;
 }
